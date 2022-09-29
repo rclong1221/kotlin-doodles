f0a48a87b98576a85fa87e97f7bd245687d83721
Roux Long <rclong1221@gmail.com>
2022-09-28 23:22:49 -0700

    add topbar and input form

--- NoteApp/app/src/main/java/com/example/noteapp/MainActivity.kt
+++ NoteApp/app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.noteapp.screens.NoteScreen
 import com.example.noteapp.ui.theme.NoteAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +23,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     NoteAppTheme {
-        Greeting("Android")
     }
 }
---
+++ NoteApp/app/src/main/java/com/example/noteapp/screens/NoteScreen.kt
@@ -0,0 +1,56 @@
+package com.example.noteapp.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.noteapp.R
+import com.example.noteapp.components.NoteInputText
+
+@Composable
+fun NoteScreen() {
+    var title by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier.padding(6.dp)
+    ) {
+        TopAppBar(
+            title = {
+                Text(
+                    text = stringResource(id = R.string.app_name)
+                )
+                    },
+            actions = {
+                Icon(
+                    imageVector = Icons.Rounded.Notifications,
+                    contentDescription = "Notification Icon"
+                )
+            },
+            backgroundColor = Color(0xFFDADFE3)
+        )
+    }
+    
+    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        NoteInputText(text = title, label = "Title", onTextChange = {})
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NoteScreenPreview() {
+    NoteScreen()
+}
